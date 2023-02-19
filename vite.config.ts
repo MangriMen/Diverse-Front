8450b276c5c0b9c1cb93822db6df540a596f3d88
MangriMen <49300253+MangriMen@users.noreply.github.com>
2023-02-19 15:39:37 +0700

    feat: VAL-10 | change folder from 'dist' to 'build' (#11)

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import path from 'path';
+
 // https://vitejs.dev/config/
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
+  build: {
+    outDir: path.join(__dirname, 'build'),
+  },
   plugins: [react(), tsconfigPaths()],
 });
