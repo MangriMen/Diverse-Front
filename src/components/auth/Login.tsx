37f9cdb4921980d476554bd0ce0b9d33e8ed8736
Danil <freeestyler231@gmail.com>
2023-02-21 20:00:05 +0700

    feat: DF-3 | create styles file with styled components

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,81 +1,38 @@
-import {
-  Box,
-  Button,
-  Container,
-  TextField,
-  Typography,
-  styled,
-} from '@mui/material';
+import { Button, Container, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
-const StyledInput = styled(TextField)({
-  '& .MuiFilledInput-root': {
-    borderRadius: 4,
-    backgroundColor: '#141618',
-    borderBottom: '2px solid #4f84c0',
-    color: '#d9d9d9',
-  },
-  '& .MuiFormLabel-root': {
-    color: '#d9d9d9',
-  },
-  '& .MuiFormLabel-root.Mui-focused': {
-    color: '#4f84c0',
-  },
-});
-
-const StyledButton = styled(Button)({
-  backgroundColor: '#4f84c0',
-  '&:hover': {
-    backgroundColor: '3073bf',
-  },
-});
+import { FormContainer, StyledBox, StyledButton, StyledInput } from './styles';
 
 export const Login = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'auth' });
 
   return (
     <Container maxWidth="lg" sx={{ height: '100%' }}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100%',
-        }}
-      >
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          <Typography variant="h1" fontWeight="bold" sx={{ mb: '1rem' }}>
+      <StyledBox>
+        <FormContainer>
+          <Typography variant="h1" fontWeight="bold">
             {'Diverse'}
           </Typography>
 
           <StyledInput
             label={t('emailPlaceholder')}
             variant="filled"
-            sx={{ mb: '1rem' }}
+            helperText=" "
             InputProps={{ disableUnderline: true }}
           />
           <StyledInput
             label={t('passwordPlaceholder')}
             variant="filled"
-            sx={{ mb: '1rem' }}
+            helperText=" "
             InputProps={{ disableUnderline: true }}
           />
 
-          <StyledButton variant="contained" sx={{ mb: '1rem' }}>
-            {t('logIn')}
-          </StyledButton>
+          <StyledButton variant="contained">{t('logIn')}</StyledButton>
           <Typography align="center">{t('youDontHaveAnAccount')}</Typography>
-          <Button variant="text" sx={{ padding: '0' }}>
-            {t('signUp')}
-          </Button>
-        </Box>
-      </Box>
+        </FormContainer>
+
+        <Button variant="text">{t('signUp')}</Button>
+      </StyledBox>
     </Container>
   );
 };
---
+++ src/components/auth/styles.tsx
@@ -0,0 +1,41 @@
+import { Box, Button, TextField, styled } from '@mui/material';
+
+export const StyledInput = styled(TextField)({
+  '& .MuiFilledInput-root': {
+    borderRadius: 4,
+    backgroundColor: '#141618',
+    borderBottom: '2px solid #4f84c0',
+    color: '#d9d9d9',
+  },
+  '& .MuiFormLabel-root': {
+    color: '#d9d9d9',
+  },
+  '& .MuiFormLabel-root.Mui-focused': {
+    color: '#4f84c0',
+  },
+});
+
+export const StyledButton = styled(Button)({
+  backgroundColor: '#4f84c0',
+  '&:hover': {
+    backgroundColor: '3073bf',
+  },
+});
+
+export const StyledBox = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+});
+
+export const FormContainer = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+});
+
+export const StyledTextButton = styled(Button)({
+  padding: 'none',
+});
