1aea6669a17b5bcc6afc7839e7d72732a7cb42a5
Danil <freeestyler231@gmail.com>
2023-02-21 19:27:10 +0700

    feat: DF-3 | add translation

---
+++ src/configs/i18next.ts
@@ -0,0 +1,14 @@
+import i18n from 'i18next';
+import { ru } from 'locales/ru';
+import { initReactI18next } from 'react-i18next';
+
+const resources = {
+  ru,
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  fallbackLng: ['en', 'ru'],
+});
+
+export { i18n };
