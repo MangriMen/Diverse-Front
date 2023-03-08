3c19e0d6563e7e56d12fe771807b378de8f5da9f
Danil Menchshikov <49301790+FFreestyler@users.noreply.github.com>
2023-03-08 18:58:48 +0700

    feat: DF-8 create header (#23)
    * feat: DF-8 | create app bar
    
    * feat: DF-8 | add menu, add styled components
    
    * feat: DF-8 | add display username
    
    * feat: DF-8 | change brackets to backticks
    
    * feat: DF-8 | change to styled components
    
    * feat: DF-8 | fix appbar movement
    
    * feat: DF-8 | fix re-render menu items, add functions to buttons
    
    * feat: DF-8 | fix
    
    * feat: DF-8 | fix name

---
+++ src/components/header/styles.tsx
@@ -0,0 +1,51 @@
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Toolbar,
+  Typography,
+  styled,
+} from '@mui/material';
+
+export const StyledAppBar = styled(AppBar)`
+  position: static;
+  display: flex;
+  flex-direction: row;
+  height: 48px;
+  box-shadow: none;
+  border-bottom: 1px solid;
+  border-color: ${props => props.theme.palette.common.third};
+  background: ${props => props.theme.palette.primary.light};
+`;
+
+export const StyledLogo = styled(Box)`
+  max-height: 28px;
+` as typeof Box;
+
+export const StyledToolbar = styled(Toolbar)`
+  justify-content: space-between;
+  max-height: 48px;
+  ${props => props.theme.breakpoints.up('sm')} {
+    min-height: 0;
+  }
+  ${props => props.theme.breakpoints.up('xs')} {
+    min-height: 0;
+  }
+`;
+
+export const StyledUserBox = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const StyledAvatar = styled(Avatar)`
+  width: 32px;
+  height: 32px;
+`;
+
+export const StyledUsername = styled(Typography)`
+  ${props => props.theme.breakpoints.down('sm')} {
+    display: none;
+  }
+`;
