a0707fda9133a8758dcf1230266bfa2622f0358b
Danil <freeestyler231@gmail.com>
2023-03-08 16:47:24 +0700

    feat: DF-8 | change to styled components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,6 @@
 import diverseText from 'assets/images/diverseText.svg';
 import {
   Avatar,
-  Box,
   Container,
   IconButton,
   Menu,
@@ -13,7 +12,13 @@ import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectIsAuth, selectUser } from 'ducks/auth/selectors';
 
-import { StyledAppBar, StyledLogo } from './styles';
+import {
+  StyledAppBar,
+  StyledAvatar,
+  StyledLogo,
+  StyledToolbar,
+  StyledUserBox,
+} from './styles';
 
 const settings = ['Profile', 'Logout'];
 
@@ -33,53 +38,42 @@ export const Header = () => {
 
   return (
     <StyledAppBar>
-      <Container
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}
-      >
-        <StyledLogo component="img" src={diverseText} alt="Diverse" />
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-          }}
-        >
-          {isAuth ? (
-            <>
-              <Typography>{user?.username}</Typography>
-              <Tooltip title="Open">
-                <IconButton onClick={handleOpenUserMenu} disableRipple>
-                  <Avatar src="src/assets/images/lucy.jpg" />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                anchorEl={anchorElUser}
-                open={!!anchorElUser}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map(settings => (
-                  <MenuItem key={settings}>
-                    <Typography>{settings}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </>
-          ) : (
-            <>
-              <Typography>{''}</Typography>
-              <Tooltip title="Open">
-                <IconButton onClick={handleOpenUserMenu} disableRipple>
-                  <Avatar />
-                </IconButton>
-              </Tooltip>
-            </>
-          )}
-        </Box>
+      <Container>
+        <StyledToolbar>
+          <StyledLogo component="img" src={diverseText} alt="Diverse" />
+          <StyledUserBox>
+            {isAuth ? (
+              <>
+                <Typography>{user?.username}</Typography>
+                <Tooltip title="Open">
+                  <IconButton onClick={handleOpenUserMenu} disableRipple>
+                    <StyledAvatar src="src/assets/images/lucy.jpg" />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  anchorEl={anchorElUser}
+                  open={!!anchorElUser}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map(settings => (
+                    <MenuItem key={settings}>
+                      <Typography>{settings}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            ) : (
+              <>
+                <Typography>{''}</Typography>
+                <Tooltip title="Open">
+                  <IconButton onClick={handleOpenUserMenu} disableRipple>
+                    <Avatar />
+                  </IconButton>
+                </Tooltip>
+              </>
+            )}
+          </StyledUserBox>
+        </StyledToolbar>
       </Container>
     </StyledAppBar>
   );
