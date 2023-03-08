8fb6d512a0258274c4ee4be16b7e2c133d983690
Danil <freeestyler231@gmail.com>
2023-03-08 16:46:58 +0700

    feat: DF-8 | change brackets to backticks

--- src/components/auth/styles.tsx
+++ src/components/auth/styles.tsx
@@ -1,76 +1,76 @@
 import { Box, Button, Container, TextField, styled } from '@mui/material';
 
-export const StyledContainer = styled(Container)({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  minHeight: '100%',
-});
+export const StyledContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100%;
+`;
 
-export const StyledInput = styled(TextField)(({ theme }) => ({
-  '& .MuiFilledInput-root': {
-    borderRadius: 4,
-    backgroundColor: theme.palette.common.third,
-    borderBottom: '2px solid',
-    borderColor: theme.palette.secondary.main,
-    '&.Mui-focused': {
-      backgroundColor: theme.palette.primary.dark,
-    },
-  },
-  '& .MuiFormLabel-root.Mui-focused': {
-    color: theme.palette.secondary.main,
-  },
-}));
+export const StyledInput = styled(TextField)`
+  & .MuiFilledInput-root {
+    border-radius: 4;
+    background-color: ${props => props.theme.palette.common.third};
+    border-bottom: 2px solid;
+    border-color: ${props => props.theme.palette.secondary.main};
+    &.Mui-Focused {
+      background-color: ${props => props.theme.palette.primary.dark};
+    }
+  }
+  & .MuiFormLabel-root.Mui-focused {
+    color: ${props => props.theme.palette.secondary.main};
+  }
+`;
 
-export const StyledFormBox = styled(Box)({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '1rem',
-});
+export const StyledFormBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
 
-export const StyledFormContainer = styled(Box)({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  padding: '8px 0',
-  gap: 'clamp( 4rem, 3.229813664596273rem + 4.968944099378882vw, 8rem )',
-});
+export const StyledFormContainer = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 8px 0;
+  gap: clamp(4rem, 3.229813664596273rem + 4.968944099378882vw, 8rem);
+`;
 
-export const StyledTextButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.secondary.main,
-  fontSize: '16px',
-  padding: '0 4px',
-  '&:hover': {
-    background: '#ffffff0f',
-  },
-  '&:focus-visible': {
-    outline: '2px solid',
-    outlineColor: 'white',
-  },
-}));
+export const StyledTextButton = styled(Button)`
+  color: theme.palette.secondary.main;
+  font-size: 16px;
+  padding: 0 4px;
+  &:hover {
+    background: #ffffff0f;
+  }
+  ,
+  &:focus-visible {
+    outline: 2px solid;
+    outline-color: white;
+  }
+`;
 
-export const StyledButton = styled(Button)({
-  padding: '4px 16px',
-  fontSize: '18px',
-  '&:focus-visible': {
-    outline: '2px solid',
-  },
-});
+export const StyledButton = styled(Button)`
+  padding: 4px 16px;
+  font-size: 18px;
+  &:focus-visible {
+    outline: 2px solid;
+  }
+`;
 
-export const StyledSwitchActionBox = styled(Box)({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-});
+export const StyledSwitchActionBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-export const StyledWrapperBox = styled(Box)({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '1rem',
-});
+export const StyledWrapperBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
 
-export const StyledAppTitled = styled(Box)({
-  width:
-    'clamp( 15.5rem, 13.430124223602483rem + 13.354037267080745vw, 26.25rem )',
-}) as typeof Box;
+export const StyledAppTitled = styled(Box)`
+  width: clamp(15.5rem, 13.430124223602483rem + 13.354037267080745vw, 26.25rem);
+` as typeof Box;
--- src/components/header/styles.tsx
+++ src/components/header/styles.tsx
@@ -1,16 +1,38 @@
-import { AppBar, Box, styled } from '@mui/material';
+import { AppBar, Avatar, Box, Toolbar, styled } from '@mui/material';
 
-export const StyledAppBar = styled(AppBar)(({ theme }) => ({
-  position: 'static',
-  display: 'flex',
-  flexDirection: 'row',
-  height: '48px',
-  boxShadow: 'none',
-  borderBottom: '1px solid',
-  borderColor: theme.palette.common.third,
-  background: theme.palette.primary.light,
-}));
+export const StyledAppBar = styled(AppBar)`
+  position: static;
+  display: flex;
+  flex-direction: row;
+  height: 48px;
+  box-shadow: none;
+  border-bottom: 1px solid;
+  border-color: ${props => props.theme.palette.common.third};
+  background: ${props => props.theme.palette.primary.light};
+`;
 
-export const StyledLogo = styled(Box)({
-  maxHeight: '28px',
-}) as typeof Box;
+export const StyledLogo = styled(Box)`
+  max-height: 28px;
+` as typeof Box;
+
+export const StyledToolbar = styled(Toolbar)`
+  justify-content: space-between;
+  max-height: 48px;
+  ${props => props.theme.breakpoints.up('sm')} {
+    min-height: 0;
+  }
+  ${props => props.theme.breakpoints.up('xs')} {
+    min-height: 0;
+  }
+`;
+
+export const StyledUserBox = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const StyledAvatar = styled(Avatar)`
+  width: 32px;
+  height: 32px;
+`;
