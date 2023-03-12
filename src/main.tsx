db7f1574e2c3952b0ac5441ba8b74741c81f235c
MangriMen <49300253+MangriMen@users.noreply.github.com>
2023-03-12 15:53:10 +0700

    ref: DF-29 | Move base css to index.html (#31)

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import 'configs/i18next';
-import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'store';
 
