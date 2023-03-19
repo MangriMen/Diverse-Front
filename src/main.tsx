b24c32f08c9701681955c6992f02ef8d861652c8
MangriMen <49300253+MangriMen@users.noreply.github.com>
2023-03-20 00:55:20 +0700

    ref: DF-27 | remove non-existent index.css import

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from 'store';
 
-import './index.css';
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
