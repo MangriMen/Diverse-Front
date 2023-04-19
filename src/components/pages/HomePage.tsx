c3ba74e3907ea033a19b9577f8e2631adbb8270f
MangriMen <49300253+MangriMen@users.noreply.github.com>
2023-04-20 06:24:16 +0700

    feat: DF-5 | fix home page post display

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,12 +1,10 @@
 import { Box, CircularProgress } from '@mui/material';
 import { DefaultFetchFade } from 'components/common/LoaderPage';
 import { Post } from 'components/post/Post';
-import { API_BASE_URL } from 'consts/endpoints';
 import { selectUser } from 'ducks/auth/selectors';
 import { useGetPostsQuery } from 'ducks/post/api';
 import { ReactNode, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { PostModel } from 'types/post';
 
 import { StyledContainer } from './styles';
 
@@ -25,17 +23,9 @@ export const HomePage = () => {
     useState<ReactNode[]>();
 
   useEffect(() => {
-    if (data?.data != undefined) {
-      setUserSubscriptionPosts(
-        data?.data.map(value => {
-          const preparedPost: PostModel = {
-            ...value,
-            content: `${API_BASE_URL}${value.content}`,
-          };
-          return <Post key={value.id} post={preparedPost} />;
-        }),
-      );
-    }
+    setUserSubscriptionPosts(
+      data?.data.map(post => <Post key={post.id} post={post} />),
+    );
   }, [data?.data]);
 
   return (
