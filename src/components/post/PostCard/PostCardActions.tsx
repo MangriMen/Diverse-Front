2b1d1fa767876b055dde4f360a28e58d4738bd65
MangriMen <49300253+MangriMen@users.noreply.github.com>
2023-05-07 01:25:36 +0700

    feat: DF-53 | move out modal to global provider

--- src/components/post/PostCard/PostCardActions.tsx
+++ src/components/post/PostCard/PostCardActions.tsx
@@ -1,39 +1,12 @@
-import {
-  StyledActionBox as PostActions,
-  PostButton,
-  PostProps,
-} from 'components/post';
+import { StyledActionBox as PostActions, PostProps } from 'components/post';
 import { PostLike } from 'components/post/Like';
-import { getShareUrl } from 'helpers/post';
-import { useState } from 'react';
-import { useTranslation } from 'react-i18next';
 
-import { ShareDialog } from '../ShareDialog';
+import { ShareButton } from './ShareButton';
 
 export const PostCardActions = ({ post }: PostProps) => {
-  const { t } = useTranslation('translation', { keyPrefix: 'post' });
-
-  const postURL = getShareUrl(post);
-
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
     <PostActions>
-      <ShareDialog
-        keepMounted
-        open={open}
-        onClose={handleClose}
-        value={postURL}
-      />
-      <PostButton onClick={handleOpen}>{t('share')}</PostButton>
+      <ShareButton post={post} />
       <PostLike post={post} />
     </PostActions>
   );
