41abd2fe47dd69525ff883721f825fe798e0e8c1
MangriMen <49300253+MangriMen@users.noreply.github.com>
2023-06-09 12:35:07 +0700

    feat: DF-102 | add sending comment on enter

--- src/components/post/PostCard/PostCardInput.tsx
+++ src/components/post/PostCard/PostCardInput.tsx
@@ -1,9 +1,14 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import SendIcon from '@mui/icons-material/Send';
 import { IconButton, InputBase, Paper, styled } from '@mui/material';
 import { PostProps } from 'components/post';
+import { POST_INPUT_MAX_ROWS } from 'consts';
 import { useSendCommentMutation } from 'ducks/comment/api';
-import { ChangeEvent, FormEvent, useCallback, useState } from 'react';
+import { KeyboardEvent, useState } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+
+import { commentValidator } from './schemas';
 
 const PaperStyled = styled(Paper)`
   display: flex;
@@ -17,42 +22,65 @@ const InputStyled = styled(InputBase)`
   font-size: 16px;
 `;
 
+interface InputValues {
+  content: string;
+}
+
+const defaultValues = {
+  content: '',
+};
+
 export const PostCardInput = ({ post }: PostProps) => {
   const { t } = useTranslation('translation', { keyPrefix: 'comment' });
 
-  const [isSendDisabled, setIsSendDisabled] = useState(false);
+  const { control, handleSubmit, reset } = useForm<InputValues>({
+    defaultValues: defaultValues,
+    resolver: yupResolver(commentValidator),
+  });
 
-  const [commentText, setCommentText] = useState('');
+  const [isSendDisabled, setIsSendDisabled] = useState(false);
 
   const [sendComment] = useSendCommentMutation();
 
-  const handleInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setCommentText(e.target.value);
-  }, []);
+  const handleInputEnter = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
+      handleSubmit(handleSendComment)();
+      return false;
+    }
+  };
 
-  const handleSendComment = useCallback(
-    async (e: FormEvent) => {
-      e.preventDefault();
+  const handleSendComment: SubmitHandler<InputValues> = async data => {
+    setIsSendDisabled(true);
 
-      setIsSendDisabled(true);
+    await sendComment({
+      path: { post: post.id },
+      body: data,
+    });
 
-      await sendComment({
-        path: { post: post.id },
-        body: { content: commentText },
-      });
+    setIsSendDisabled(false);
 
-      setCommentText('');
-      setIsSendDisabled(false);
-    },
-    [commentText, post.id, sendComment],
-  );
+    reset();
+  };
 
   return (
-    <PaperStyled component="form" onSubmit={handleSendComment} elevation={24}>
-      <InputStyled
-        placeholder={t('writeComment') ?? ''}
-        value={commentText}
-        onChange={handleInputChange}
+    <PaperStyled
+      component="form"
+      onSubmit={handleSubmit(handleSendComment)}
+      elevation={24}
+    >
+      <Controller
+        control={control}
+        name="content"
+        render={({ field }) => (
+          <InputStyled
+            multiline
+            maxRows={POST_INPUT_MAX_ROWS}
+            placeholder={t('writeComment') ?? ''}
+            {...field}
+            onKeyDown={handleInputEnter}
+          />
+        )}
       />
       <IconButton
         type="submit"
