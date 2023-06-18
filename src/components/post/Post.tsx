de3999e3d3668a15ffe240ab4006f6a61e2b33a4
MangriMen <49300253+MangriMen@users.noreply.github.com>
2023-06-19 03:59:01 +0700

    feat: DF-52 | add fullscreen post look

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,5 +1,6 @@
 import { PostProps } from 'components/post';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { PostCardActions } from './PostCard/PostCardActions';
 import { PostCardComments } from './PostCard/PostCardComments';
@@ -15,18 +16,32 @@ import {
 } from './PostCard/styles';
 
 export const Post = ({ post, size = 'default' }: PostProps) => {
+  const isDisplayContentButton = size !== 'fullscreen';
+  const isDisplayContent = size === 'fullscreen' || size === 'default';
+
+  const navigate = useNavigate();
+
   const [expanded, setExpanded] = useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
+  const handleContentClick = () => {
+    navigate(`/post/${post.id}`);
+  };
+
   return (
     <PostCardStyled size={size} elevation={0}>
-      <PostCardActionArea>
+      {isDisplayContentButton && (
+        <PostCardActionArea onClick={handleContentClick}>
+          <PostCardMeida size={size} image={post.content} />
+        </PostCardActionArea>
+      )}
+      {!isDisplayContentButton && (
         <PostCardMeida size={size} image={post.content} />
-      </PostCardActionArea>
-      {size === 'default' && (
+      )}
+      {isDisplayContent && (
         <PostCardContent size={size}>
           <PostCardHeader post={post} />
           <PostCardDescription
